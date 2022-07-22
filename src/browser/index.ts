9719b58d646cc2a62fe50c3cd6fda525480b6851
suressk <sure_ssk@outlook.com>
2022-07-22 22:23:55 +0800

    feat: add some browser funcs

---
+++ src/browser/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Copy text to clipboard
+ * 将文本复制到剪贴板
+ * @param text
+ * @returns
+ */
+export const copyToClipboard = (text: string) => navigator.clipboard.writeText(text)
+
+/**
+ * Get user selected text
+ * @returns
+ */
+export const getSelectedText = () => window.getSelection()?.toString()
+
+/**
+ * 检测当前环境是否是黑暗模式
+ */
+export const isDarkMode = window?.matchMedia('(prefers-color-scheme: dark)').matches
+
+/**
+ * 检测当前环境是否是黑暗模式
+ */
+export const isAppleDevice = (/Mac|iPhone|iPad|iPod/ig).test(navigator.userAgent)
