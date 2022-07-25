09698d9883ea47d6d3335a71d4e840770238ffc1
suressk <sure_ssk@outlook.com>
2022-07-25 16:11:23 +0800

    chore: rename directory

--- src/.internal/constants.ts
+++ src/.internal/constants.ts
@@ -22,3 +22,12 @@ export const UINT8_TAG = '[object Uint8Array]'
 export const UINT8CLAMPED_TAG = '[object Uint8ClampedArray]'
 export const UINT16_TAG = '[object Uint16Array]'
 export const UINT32_TAG = '[object Uint32Array]'
+
+export const randomLetters = [
+  '0', '1', '2', '3', '4', '5',
+  '6', '7', '8', '9', 'A', 'B',
+  'C', 'D', 'E', 'F', 'G', 'H',
+  'I', 'J', 'K', 'L', 'M', 'N',
+  'O', 'P', 'Q', 'R', 'S', 'T',
+  'U', 'V', 'W', 'X', 'Y', 'Z'
+]
